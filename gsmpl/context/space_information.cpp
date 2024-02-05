179e6ca5a6c9927c3f0132cb8e1248ecbd4845f9
dsh <dushaohe@zhiyuan-robot.com>
2024-02-05 15:50:39 +0800

    add log

--- gsmpl/context/space_information.cpp
+++ gsmpl/context/space_information.cpp
@@ -31,13 +31,17 @@ SpaceInformationBase::FlexibleTools SpaceInformation::creatFlexibleTools(
 PathSimplifier::SolutionRecord SpaceInformation::simplifyPath(
     const Path& rawPath, const PathSimplifierParamters& param) {
     PathSimplifier::SolutionRecord solution;
+    std::cout << "simplifyPath 1" << std::endl;
     pt.rrtStarPathLength = pathLength(rawPath, distance);
+    std::cout << "simplifyPath 2" << std::endl;
     pt.rrtStarPathSize = rawPath.size();
+    std::cout << "simplifyPath 3" << std::endl;
     solution.simplified = path_simplifier.reduceVertices(
         rawPath, param.step_size_jps, param.stepSizeTcp,
         param.reduceVerticesMaxSteps, param.maxEmptySteps,
         param.reduceVerticesRangeRatio);
     
+    std::cout << "simplifyPath 4" << std::endl;
     LOGD("simplified waypoints size:%d",
          static_cast<int>(solution.simplified.size()));
     std::cout << "simplified waypoints size: "
@@ -47,7 +51,7 @@ PathSimplifier::SolutionRecord SpaceInformation::simplifyPath(
     solution.smoothed = path_simplifier.smoothBSpline(
         solution.simplified, param.smoothBSplineMaxSteps,
         param.smoothBSplineMinChange, param.step_size_jps, param.stepSizeTcp);
-    
+    std::cout << "simplifyPath 5" << std::endl;
     LOGD("smoothed waypoints size:%d",
          static_cast<int>(solution.smoothed.size()));
     std::cout << "smoothed waypoints size: "
@@ -55,6 +59,7 @@ PathSimplifier::SolutionRecord SpaceInformation::simplifyPath(
               << pathLength(solution.smoothed, distance) << std::endl;
 
     solution.simplified_2 = solution.smoothed;
+    std::cout << "simplifyPath 6" << std::endl;
     int i = 0;
     while (solution.simplified_2.size() > 10 && i < 10) {
         solution.simplified_2 = path_simplifier.reduceVertices(
@@ -64,6 +69,7 @@ PathSimplifier::SolutionRecord SpaceInformation::simplifyPath(
         std::cout << "simplifyed once more!!!" << std::endl;
         i++;
     }
+    std::cout << "simplifyPath 7" << std::endl;
     
     pt.finalPathLength = pathLength(solution.simplified_2, distance);
     std::cout << "simplified_2 size: "
@@ -71,7 +77,7 @@ PathSimplifier::SolutionRecord SpaceInformation::simplifyPath(
               << pathLength(solution.simplified_2, distance)
               << std::endl;
     pt.pathSize = solution.simplified_2.size();
-
+    std::cout << "simplifyPath 8" << std::endl;
     return solution;
 }
 } // namespace gsmpl
