b231d5bc9d305418895fdd3e0c0afe0fb544b072
dsh <dushaohe@zhiyuan-robot.com>
2024-03-22 16:17:09 +0800

    add path simplifier

--- gsmpl/planner_interface.cpp
+++ gsmpl/planner_interface.cpp
@@ -93,6 +93,26 @@ bool PlannerInterface::allocPlanner(const PlannerContext &context) {
 
 //     return false;
 // }
+// bool PlannerInterface::plan(PlannerSolution &solution) {
+//     auto startTime1 = std::chrono::steady_clock::now();
+//     solution = planner_->solve();
+//     auto endTime1 = std::chrono::steady_clock::now();
+//     record_ = planner_->plannerRecord();
+//     std::cout << "solution.isValid() " << solution.isValid() << std::endl;
+
+//     if (solution.isValid()) {
+//         auto startTime2 = std::chrono::steady_clock::now();
+//         pt.rrtSolver =
+//             std::chrono::duration<float>(endTime1 - startTime1).count();
+
+//         pt.print();
+//         record_.addPath(solution.path, "solution.path");
+//         // vf_(record_);
+//         return true;
+//     }
+
+//     return false;
+// }
 bool PlannerInterface::plan(PlannerSolution &solution) {
     auto startTime1 = std::chrono::steady_clock::now();
     solution = planner_->solve();
@@ -102,11 +122,31 @@ bool PlannerInterface::plan(PlannerSolution &solution) {
 
     if (solution.isValid()) {
         auto startTime2 = std::chrono::steady_clock::now();
+        auto simplifierSolution =
+            si_->simplifyPath(solution.path, param_.path_simplifier);
+        auto endTime2 = std::chrono::steady_clock::now();
+
+        std::cout << "PlannerInterface Trajectory start" << std::endl;
+        solution.trajectory = Trajectory(
+            si_->local_planner, simplifierSolution.simplified_2,
+            param_.traj_processing.step_size_jps,
+            param_.traj_processing.stepSizeTcp, param_.traj_processing.dt);
+        std::cout << "PlannerInterface Trajectory end" << std::endl;
+        
+        solution.path = simplifierSolution.simplified_2;
+
+        auto endTime3 = std::chrono::steady_clock::now();
+        pt.planner =
+            std::chrono::duration<float>(endTime3 - startTime1).count();
         pt.rrtSolver =
             std::chrono::duration<float>(endTime1 - startTime1).count();
-
+        pt.pathProcessing =
+            std::chrono::duration<float>(endTime2 - startTime2).count();
         pt.print();
-        record_.addPath(solution.path, "solution.path");
+        record_.addPath(simplifierSolution.simplified, "simplified");
+        record_.addPath(simplifierSolution.smoothed, "smoothed");
+        record_.addPath(simplifierSolution.simplified_2, "simplified_2");
+        record_.addPath(solution.trajectory.dense_waypoints, "dense_waypoints");
         // vf_(record_);
         return true;
     }
